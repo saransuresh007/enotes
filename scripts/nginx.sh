a9677bd39987317f953822448f44d34fd9fb9ec4
saran KS <sks@presidio.com>
2023-04-03 23:36:05 +0530

    enotes initial commit

---
+++ scripts/nginx.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/bash
+
+sudo systemctl daemon-reload
+sudo rm -f /etc/nginx/sites-enabled/default
+
+sudo cp /home/ubuntu/enotes/nginx/nginx.conf /etc/nginx/sites-available/Enotes_Management_System
+sudo ln -s /etc/nginx/sites-available/Enotes_Management_System /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/Enotes_Management_System /etc/nginx/sites-enabled
+#sudo nginx -t
+sudo gpasswd -a www-data ubuntu
+sudo systemctl restart nginx
+
