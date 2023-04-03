a706a2dd577989b4685c36f22c957dd6ed33e017
saran KS <sks@presidio.com>
2023-04-04 01:44:16 +0530

    error gnix 2

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/enotes/nginx/nginx.conf /etc/nginx/sites-available/Enotes_Management_System/
+sudo cp /home/ubuntu/enotes/nginx/nginx.conf /etc/nginx/sites-available/Enotes_Management_System
 sudo ln -s /etc/nginx/sites-available/Enotes_Management_System /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/Enotes_Management_System /etc/nginx/sites-enabled
 #sudo nginx -t
