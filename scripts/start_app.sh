693d8bec4f5fec27e8e11607c5d5f15e9902ea85
saran KS <sks@presidio.com>
2023-04-04 16:56:46 +0530

    error gunicorn

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -12,8 +12,8 @@ sudo service nginx restart
 #sudo tail -f /var/log/nginx/error.log
 #sudo nginx -t
 #sudo systemctl restart gunicorn
-#sudo systemctl status gunicorn
-#sudo systemctl status nginx
+sudo systemctl status gunicorn
+sudo systemctl status nginx
 # Check the status
 #systemctl status gunicorn
 # Restart:
