44fc846c43dc7cf0687f04a3760eaac81e4309e7
saran KS <sks@presidio.com>
2023-04-04 18:36:04 +0530

    rever gunicorn

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# virtualenv /home/ubuntu/env
-# source /home/ubuntu/env/bin/activate
-# pip install -r /home/ubuntu/enotes/requirements.txt
-
-virtualenv env
-source /home/ubuntu/enotes/env/bin/activate
+virtualenv /home/ubuntu/env
+source /home/ubuntu/env/bin/activate
 pip install -r /home/ubuntu/enotes/requirements.txt
